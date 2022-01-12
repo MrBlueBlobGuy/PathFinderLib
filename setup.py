a0bdd8538f7bba40b21f61407e0109171c2afd91
MrBlue <aternoshostingmc@gmail.com>
2022-01-12 11:05:12 +0530

    Changed Name from Pathlib to PathFinderLib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='Pathlib',
+    name='PathFinderlib',
     packages=find_packages(),
     version='0.1.0',
     description='A package to implement Pathfinding into your games',
